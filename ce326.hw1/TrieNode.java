cfea6c76316962255ebee4a02f5a73ac54f38c0b
ddiannelos <jdian18@gmail.com>
2022-03-14 18:53:44 +0200

    TrieNode: completed, Trie->insert,remove,find: completed

--- ce326.hw1/TrieNode.java
+++ ce326.hw1/TrieNode.java
@@ -2,7 +2,7 @@ public class TrieNode {
     public TrieNode[] children = new TrieNode[26];
     public String content = new String();
     public boolean isWord;
- 
+
     public TrieNode(boolean isWord) {
         this.isWord = isWord;
     }
