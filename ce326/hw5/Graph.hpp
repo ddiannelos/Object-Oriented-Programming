8949773c1392f7c44ec4e9186fd7bb5a96822dd6
ddiannelos <jdian18@gmail.com>
2022-06-29 18:34:26 +0300

    Started Graph.hpp

---
+++ ce326/hw5/Graph.hpp
@@ -0,0 +1,285 @@
+
+#ifndef _GRAPH_HPP_ 
+#define _GRAPH_HPP_
+
+#include <list>
+#include <iterator>
+#include <typeinfo>
+
+using namespace std;
+
+template<typename T>
+struct Edge {
+    T from;
+    T to;
+    int dist;
+    Edge(T f, T t, int d) : from(f), to(t), dist(d) {
+
+    }
+    bool operator<(const Edge<T>& e) const {
+        return (dist < e.dist);
+    }
+    bool operator>(const Edge<T>& e) const {
+        return (dist > e.dist);
+    }
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream& out, const Edge<U>& e);
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const Edge<T>& e) {
+    out << e.from << " -- " << e.to << " (" << e.dist << ")";
+    return out;
+}
+
+template <typename T>
+class Graph {
+    list<Edge<T>> *edges;
+    int size;
+    bool isDirected;
+    
+    void dfs(const T& info, bool *visited, list<T> dfs);
+
+    public:
+        Graph(bool isDirectedGraph = true);
+        ~Graph();
+        bool contains(const T& info);
+        bool addVtx(const T& info);
+        bool rmvVtx(const T& info);
+        bool addEdg(const T& from, const T& to, int distance);
+        bool rmvEdg(const T& from, const T& to);
+        list<T> dfs(const T& info) const;
+        list<T> bfs(const T& info) const;
+        list<Edge<T>> mst();
+        
+        void print2DotFile(const char *filename) const;
+        list<T> dijkstra(const T& from, const T& to);
+};
+
+//***Constructor***
+template <typename T>
+Graph<T>::Graph(bool isDirectedGraph) {
+    edges = nullptr;
+    size = 0;
+    isDirected = isDirectedGraph;
+}
+
+// ***Deconstructor***
+template <typename T>
+Graph<T>::~Graph() {
+
+}
+
+// ***contains***
+template <typename T>
+bool Graph<T>::contains(const T& info) {
+    // Search in the array to find
+    // the verticle
+    for (int i = 0; i < size; i++)
+        if (edges[i].begin().from == info)
+            return true;
+
+    return false;
+}
+
+// ***addVtx***
+template <typename T>
+bool Graph<T>::addVtx(const T& info) {
+    // Check if info exists in
+    // the array
+    if (contains(info))
+        return false;
+    
+    // If the graph is empty
+    // create it and insert 
+    // the verticle
+    if (size == 0) {
+        edges = new list<Edge<T>>[++size];
+        edges[0].push_back(Edge<T>(info, info, -1));
+    }
+    // Else copy the contents of the
+    // previous list to a new one and
+    // insert the new verticle
+    else {
+        list<Edge<T>> *newlist = new list<Edge<T>>[++size];
+
+        for (int i = 0; i < size-1; i++)
+            for (typename list<Edge<T>>::iterator it = edges[i].begin(); it != edges[i].end(); it++)
+                newlist[i].push_back(it);
+            
+        newlist[size-1].push_back(Edge<T>(info, info, -1));
+        
+        delete[] edges;
+        edges = newlist;
+    }
+
+    return true;
+}
+
+// ***rmvVtx***
+template <typename T>
+bool Graph<T>::rmvVtx(const T& info) {
+    // Check if info exists in the
+    // graph
+    if (contains(info) == false)
+        return false;
+    
+    // Create a new array and
+    // copy the contents of teh
+    // previous array except info
+    list<Edge<T>> *newlist = new list<Edge<T>>[--size];
+
+    for (int i = 0, j = 0; i < size+1; i++) {
+        if (edges[i].begin().from == info) {
+            continue;
+        }
+
+        for (typename list<Edge<T>>::iterator it = edges[i].begin(); it != edges[i].end(); it++) {
+            if (it.to == info)
+                continue;
+            
+            newlist[j].push_back(it);
+        }
+
+        j++;
+    }
+
+    delete[] edges;
+    edges = newlist;
+
+    return true;
+}
+
+// ***addEdg***
+template <typename T>
+bool Graph<T>::addEdg(const T& from, const T& to, int cost) {
+    // Check if to exists in the graph
+    if (contains(to) == false)
+        return false;
+
+    // Check if from exists in the graph and
+    // if it does, add an edge
+    for (int i = 0; i < size; i++)
+        if (edges[i].begin().from == from) {
+            // Check if the edge exists
+            for (typename list<Edge<T>>::iterator it = edges[i].begin(); it != edges[i].end(); it++)
+                if (it.to == to)
+                    return false;
+            
+            if (edges[i].begin().from == edges[i].begin().to) {
+                edges[i].begin().to = to;
+                edges[i].begin().dest = cost;
+            }
+            else 
+                edges[i].push_back(Edge<T>(from, to, cost));
+            
+            // If the graph is not directed insert
+            // the edge from "to" to "from"
+            if (isDirected == false) {
+                for (int j = 0; j < size; j++)
+                    if (edges[j].begin().from == to) {
+                        if (edges[j].begin().from == edges[j].begin().to) {
+                            edges[j].begin().to = from;
+                            edges[j].begin().dest = cost;
+                        }
+                        else
+                            edges[j].push_back(Edge<T>(to, from, cost));
+                    }
+            }
+        
+            return true;
+        }
+
+    return false;
+}
+
+// ***rmvEdg***
+template <typename T>
+bool Graph<T>::rmvEdg(const T& from, const T& to) {
+    // Check if to exists in the graph
+    if (contains(to) == false)
+        return false;
+    
+    // Check if from exists in the graph
+    // and if it does, try to find the edge
+    // and remove it
+    for (int i = 0; i < size; i++)
+        if (edges[i].begin().from == from) {
+            typename list<Edge<T>>::iterator it = edges[i].begin();
+
+            for (; it != edges[i].end(); it++)
+                if (it.to == to)
+                    break;
+            
+            if (it == edges[i].end())
+                return false;
+
+            if (edges[i].size() == 1) {
+                edges[i].begin().to = edges[i].begin().from;
+                edges[i].begin().dest = -1;
+            }
+            else
+                edges[i].erase(it);
+
+            // If graph is not directed remove the
+            // edge from "to" to "from"
+            if (isDirected == false) {
+                for (int j = 0; j < size; j++)
+                    if (edges[j].begin().from == to) {
+                        it = edges[j].begin();
+
+                        for (; it != edges[j].end(); it++)
+                            if (it.to == from)
+                                break;
+                        
+                        if (edges[j].size() == 1) {
+                            edges[j].begin().to = edges[j].begin().from;
+                            edges[j].begin().dist = -1;
+                        }
+                        else
+                            edges[j].erase(it);
+
+                    }
+            }
+
+            return true;
+        }
+
+    return false;
+}
+
+// ***dfs***
+template <typename T>
+void Graph<T>::dfs(const T& info, bool visited[], list<T> dfs) {
+    // Find info, check if info
+    // is already visited and if 
+    // it is not insert it in the list
+    // and dfs every neighbor
+    for (int i = 0; i < size; i++)
+        if (edges[i].begin().from == info) {
+            if (visited[i] == true)
+                return;
+            
+            visited[i] = true;
+            dfs.push_back(edges[i].begin().from);
+
+            for (typename list<Edge<T>>::iterator it = edges[i].begin(); it != edges[i].end(); it++)
+                dfs(it.to, visited, dfs);
+        }
+}
+
+// **dfs***
+template <typename T>
+list<T> Graph<T>::dfs(const T& info) const {
+    bool visited[size];
+    list<T> dfs;
+
+    for (int i = 0; i < size; i++)
+        visited[i] = false;
+
+    dfs(info, visited, dfs);
+    
+    return dfs;
+}
+
+#endif
